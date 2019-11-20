c562f00baaa0736de69b63f68ab34052669847c7
WuMenghao <33742731+WuMenghao@users.noreply.github.com>
2019-11-20 11:54:37 +0800

    TensorFlow slim 微调demo

---
+++ Chapter03/data_prepare/data_convert.bat
@@ -0,0 +1,4 @@
+D:\ProgramData\Anaconda3\envs\deepLearningDemo\python.exe ^
+D:\workspace_py\deepLearningDemo\chapter03\data_prepare\data_convert.py -t pic/ ^
+--train-shards 2 --validation-shards 2 ^
+--num-threads 2 --dataset-name satellite
