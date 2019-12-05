19c269ec3356bded5f6e16a8c4622844e33fd627
wumenghao <tm_wmh@foxmail.com>
2019-12-05 18:11:07 +0800

    object_detection:深度学习中的目标检测

---
+++ chapter05/s_chapter02/create_pascal_tf_record.bat
@@ -0,0 +1,15 @@
+:: gen pascal_train.record
+python create_pascal_tf_record.py ^
+--data_dir=VOCtrainval_11-May-2012/VOCdevkit/ ^
+--year=VOC2012 ^
+--set=train ^
+--output_path=VOCtrainval_11-May-2012/pascal_train.record ^
+--label_map_path=D:\workspace_py\deepLearningDemo\chapter05\research\object_detection\data\pascal_label_map.pbtxt
+
+:: gen pascal_val.record
+python create_pascal_tf_record.py ^
+--data_dir=VOCtrainval_11-May-2012/VOCdevkit/ ^
+--year=VOC2012 ^
+--set=val ^
+--output_path=VOCtrainval_11-May-2012/pascal_val.record ^
+--label_map_path=D:\workspace_py\deepLearningDemo\chapter05\research\object_detection\data\pascal_label_map.pbtxt
